b140ae62e83ac2603e9acd15e7e06eb618e589bb
한양남자 <110797599+myoh0623@users.noreply.github.com>
2024-02-04 13:20:52 +0000

    Update load_model.py

--- model_inference/load_model.py
+++ model_inference/load_model.py
@@ -13,22 +13,17 @@ def keras_model():
 # model 전처리
 def preprocessing_url(url):
     IMAGE_SHAPE = (224, 224)
-    file_type = url.split('.')[-1]
-    file_name = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    user_input_image = tf.keras.utils.get_file(f'{file_name}.{file_type}',url)
+    user_input_image = tf.keras.utils.get_file(fname=url.split('/')[-1], origin=url)
     user_input_image = Image.open(user_input_image).resize(IMAGE_SHAPE)
-    image2array = np.array(user_input_image)/255.0
-    return image2array[np.newaxis, ...].tolist()
+    image2array = np.array(user_input_image) / 255.0
+    return np.expand_dims(image2array, axis=0)  # 변경된 부분
 
 def preprocessing_byte(byte_image):
-    # 입력 받은 이미지 decoding
     decoded = base64.b64decode(byte_image)
-    # BytesIO 를 이용해 image 를 open 한다. 
     IMAGE_SHAPE = (224, 224)
     user_input_image = Image.open(BytesIO(decoded)).resize(IMAGE_SHAPE)
-    image2array = np.array(user_input_image)/255.0
-    return image2array[np.newaxis, ...].tolist()
-
+    image2array = np.array(user_input_image) / 255.0
+    return np.expand_dims(image2array, axis=0)  # 변경된 부분
 
 # 추론 결과 후처리 
 def postprocessing(predicted_class):
@@ -36,16 +31,14 @@ def postprocessing(predicted_class):
     predicted_class_name = imagenet_labels[predicted_class]
     return predicted_class_name
 
-
 def main():
     url = "https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg"
     mobilenet_v2 = keras_model()
-    image = preprocessing(url=url)
+    image = preprocessing_url(url)  # 변경된 부분
     result = mobilenet_v2.predict(image)
     predicted_class = np.argmax(result[0], axis=-1)
     predicted_class_name = postprocessing(predicted_class)
     print(predicted_class_name)
-    return predicted_class_name
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
