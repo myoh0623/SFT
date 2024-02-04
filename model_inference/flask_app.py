09e092d20c0ec8b7e06f9545ee51e8b51d4b1204
한양남자 <110797599+myoh0623@users.noreply.github.com>
2024-02-04 13:18:10 +0000

    Update flask_app.py

--- model_inference/flask_app.py
+++ model_inference/flask_app.py
@@ -1,29 +1,18 @@
-# from flask import Flask, request
 from flask import Flask, request, jsonify
 from load_model import keras_model, preprocessing_byte, postprocessing
-import json
 import numpy as np
-import base64
 
 app = Flask(__name__)
+mobilenet_v2 = keras_model()  # 모델을 전역 변수로 한 번만 로드
 
-@app.route("/predict", methods=["POST"]) 
+@app.route("/predict", methods=["POST"])
 def inference():
-    # # url 을 입력 받았을때 출력하는 code
-    # mobilenet_v2 = keras_model()
-    # request_body = request.get_json()
-    # user_url =  request_body["url"]
-    # image = preprocessing(url=user_url)
-    # result = mobilenet_v2.predict(image)
-    # #################################
-    mobilenet_v2 = keras_model()
     image_data = request.form.get("image_data")
     image = preprocessing_byte(image_data)
     result = mobilenet_v2.predict(image)
     predicted_class = np.argmax(result[0], axis=-1)
     predicted_class_name = postprocessing(predicted_class)
-    result = {"predicted":str(result[0]), "class":predicted_class_name}
-    print(predicted_class_name)
+    result = {"predicted": str(result[0]), "class": predicted_class_name}
     return jsonify(result)
 
 if __name__ == "__main__":
